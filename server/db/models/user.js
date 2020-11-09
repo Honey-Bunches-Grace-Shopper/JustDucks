88e367ccbc87a5afc1fac040069b933230ac935e
Sirui Sun <catheory91@gmail.com>
2020-11-09 14:11:21 -0800

    add field to user model

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -3,6 +3,10 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const User = db.define('user', {
+  firstName:{
+    type: Sequelize.STRING,
+    allowNull: false
+   },
   email: {
     type: Sequelize.STRING,
     unique: true,
