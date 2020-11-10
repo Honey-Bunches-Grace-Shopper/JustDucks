f7144b289a19d3efd924913367219c1613377ce1
Ellen Granoff <granoff.ellen@gmail.com>
2020-11-10 15:45:11 -0500

    add products reducer

---
+++ client/store/product.js
@@ -0,0 +1,44 @@
+import axios from 'axios'
+
+/**
+ * ACTION TYPES
+ */
+
+const SET_PRODUCTS = 'SET_PRODUCTS'
+
+/**
+ * INITIAL STATE
+ */
+const initialState = []
+
+/**
+ * ACTION CREATORS
+ */
+export const setProducts = products => ({
+  type: SET_PRODUCTS,
+  products
+})
+
+/**
+ * THUNK CREATORS
+ */
+export const fetchProducts = () => async dispatch => {
+  //   try {
+  const {data} = await axios.get('/api/products')
+  dispatch(setProducts(data))
+  //   } catch (err) {
+  //       console.error("Error fetching products: ",err)
+  //   }
+}
+
+/**
+ * REDUCER
+ */
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case SET_PRODUCTS:
+      return action.products
+    default:
+      return state
+  }
+}
