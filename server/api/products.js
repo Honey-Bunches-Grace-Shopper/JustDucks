0ad585b9282ff1356ef7be6b568c12b978b59b8b
Ellen Granoff <granoff.ellen@gmail.com>
2020-11-10 15:20:59 -0500

    feature: add GET routes for /api/products

---
+++ server/api/products.js
@@ -0,0 +1,24 @@
+const router = require('express').Router()
+const {Product} = require('../db/models')
+module.exports = router
+
+//  GET  /api/products
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await Product.findAll()
+    res.json(products)
+  } catch (err) {
+    next(err)
+  }
+})
+
+//  GET  /api/products/:id
+router.get('/', async (req, res, next) => {
+  try {
+    const {productId} = req.params
+    const product = await Product.findByPk(productId)
+    res.json(product)
+  } catch (err) {
+    next(err)
+  }
+})
