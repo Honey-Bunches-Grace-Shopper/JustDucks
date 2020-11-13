c901f5d8ff64a6a58ff41a9170340ae53aaec631
Ellen Granoff <granoff.ellen@gmail.com>
2020-11-13 15:58:24 -0500

    Feature: Admin stock button in AllProducts View

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -7,7 +7,18 @@ export default class SingleProduct extends React.Component {
   }
 
   render() {
-    console.log(this.props.isAdmin)
+    let adminControls = (
+      <div className="adminControls">
+        <h3>Manage Stock</h3>
+        <button>Remove Item</button>
+        <button>Increase Quantity</button>
+        <button>Decrease Quantity</button>
+      </div>
+    )
+    let shopperControls = (
+      <button className="addToCartButton">Add To Nest</button>
+    )
+
     let {product} = this.props
     let {id, name, price, helpfulness, description} = product
     return (
@@ -16,10 +27,11 @@ export default class SingleProduct extends React.Component {
           <h2>{name}</h2>
         </Link>
         <img width="100px" className="productImage" src={product.imageUrl} />
-        <h3>{price}</h3>
-        <h3>{helpfulness}</h3>
+        <h3>${price}</h3>
+        <h3>Helpfulness: {helpfulness}</h3>
         <h3>{description}</h3>
-        <button className="addToCartButton">Add To Nest</button>
+        {this.props.admin && adminControls}
+        {this.props.admin && shopperControls}
       </li>
     )
   }
