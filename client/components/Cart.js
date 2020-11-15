7601285425a78dc5ca56aa7046e7aa91f68ee078
Katelynn Burns <katelynnburns655@gmail.com>
2020-11-15 16:03:12 -0500

    Update: Cart Working on Submitting Order

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -7,13 +7,38 @@ class Cart extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      quantity: 0,
-      id: ''
+      quantity: '',
+      id: '',
+      firstName: '',
+      lastName: '',
+      streetAddress: '',
+      city: '',
+      zipCode: '',
+      name: '',
+      ccNumber: '',
+      ssid: '',
+      cardType: 'Discover',
+      billingZip: '',
+      experation: ''
     }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   componentDidMount() {
     this.props.getCart()
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault()
+    this.props.submitCart()
+    this.setState({quantity: 0, id: ''})
+  }
+
+  handleChange(evt) {
+    console.log(evt.target)
+    this.setState({[evt.target.name]: evt.target.value})
+    console.log(this.state)
   }
 
   render() {
@@ -41,37 +66,101 @@ class Cart extends React.Component {
           <div className="shipping">
             <h4>SHIPPING INFORMATION</h4>
             <form onSubmit={this.handleSubmit}>
-              <label htmlFor="firstName">First Name:</label>
-              <input type="text" id="firstName" name="lastName" />
-              <label htmlFor="lastName">Last Name:</label>
-              <input type="text" id="lastName" name="lastName" />
-              <label htmlFor="streetAddress">Street Address:</label>
-              <input type="text" id="streetAddress" name="streetAddress" />
-              <label htmlFor="city">City</label>
-              <input type="text" id="city" name="city" />
-              <label htmlFor="zipCode">ZIP Code:</label>
-              <input type="number" id="zipCode" name="zipCode" />
-            </form>
-          </div>
-          <div>
-            <h4>INPUT CREDIT CARD</h4>
-            <form onSubmit={this.handleSubmit}>
-              <label htmlFor="name">Name:</label>
-              <input type="text" id="name" name="name" />
-              <label htmlFor="ccNumber">Credit Card Number:</label>
-              <input type="password" id="ccNumber" name="ccNumber" />
-              <label htmlFor="ssid">SSID</label>
-              <input type="password" id="ssid" name="ssid" />
-              <label htmlFor="cardType">Card Type:</label>
-              <select name="cardType" id="cardType">
-                <option value="Discover">Discover</option>
-                <option value="Visa">Visa</option>
-              </select>
-              <label htmlFor="billingZip">Billing ZIP:</label>
-              <input type="number" id="billingZip" name="billingZip" />
-              <label htmlFor="experation">Experation Date:</label>
-              <input type="month" id="experation" name="experation" />
-              <button type="submit">Submit Order</button>
+              <div>
+                <label htmlFor="firstName">First Name:</label>
+                <input
+                  type="text"
+                  id="firstName"
+                  name="lastName"
+                  value={this.state.firstName}
+                  onChange={this.handleChange}
+                />
+                <label htmlFor="lastName">Last Name:</label>
+                <input
+                  type="text"
+                  id="lastName"
+                  name="lastName"
+                  value={this.state.lastName}
+                  onChange={this.handleChange}
+                />
+                <label htmlFor="streetAddress">Street Address:</label>
+                <input
+                  type="text"
+                  id="streetAddress"
+                  name="streetAddress"
+                  value={this.state.streetAddress}
+                  onChange={this.handleChange}
+                />
+                <label htmlFor="city">City</label>
+                <input
+                  type="text"
+                  id="city"
+                  name="city"
+                  value={this.state.city}
+                  onChange={this.handleChange}
+                />
+                <label htmlFor="zipCode">ZIP Code:</label>
+                <input
+                  type="number"
+                  id="zipCode"
+                  name="zipCode"
+                  value={this.state.zipCode}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div>
+                <h4>INPUT CREDIT CARD</h4>
+                <label htmlFor="name">Name:</label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  onChange={this.handleChange}
+                  value={this.state.name}
+                />
+                <label htmlFor="ccNumber">Credit Card Number:</label>
+                <input
+                  type="password"
+                  id="ccNumber"
+                  name="ccNumber"
+                  value={this.state.ccNumber}
+                  onChange={this.handleChange}
+                />
+                <label htmlFor="ssid">SSID</label>
+                <input
+                  type="password"
+                  id="ssid"
+                  name="ssid"
+                  value={this.state.ssid}
+                  onChange={this.handleChange}
+                />
+                <label htmlFor="cardType">Card Type:</label>
+                <select
+                  name="cardType"
+                  id="cardType"
+                  onChange={this.handleChange}
+                >
+                  <option value="Discover">Discover</option>
+                  <option value="Visa">Visa</option>
+                </select>
+                <label htmlFor="billingZip">Billing ZIP:</label>
+                <input
+                  type="number"
+                  id="billingZip"
+                  name="billingZip"
+                  onChange={this.handleChange}
+                  value={this.state.billingZip}
+                />
+                <label htmlFor="experation">Experation Date:</label>
+                <input
+                  type="month"
+                  id="experation"
+                  name="experation"
+                  value={this.state.experation}
+                  onChange={this.handleChange}
+                />
+                <button type="submit">Submit Order</button>
+              </div>
             </form>
           </div>
         </div>
@@ -83,7 +172,8 @@ class Cart extends React.Component {
 const mapDispatch = dispatch => ({
   getCart: () => dispatch(fetchCart()),
   removeItem: orderId => dispatch(removeItem(orderId)),
-  changeQuantity: (quantity, id) => dispatch(changeQuantity(quantity, id))
+  changeQuantity: (quantity, id) => dispatch(changeQuantity(quantity, id)),
+  submitCart: () => dispatch(submitCart())
 })
 
 const mapState = state => ({
