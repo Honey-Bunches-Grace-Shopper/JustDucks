2d04a66968b2964842156babe48b643ed2f8cfb2
Ellen Granoff <granoff.ellen@gmail.com>
2020-11-16 18:37:12 -0500

    Feature: Guest add to cart

---
+++ client/store/guestCart.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+
+//actions types
+
+const SET_GUEST_CART = 'SET_GUEST_CART'
+
+//initial state
+
+const initialState = []
+
+//action creators
+
+export const initialCart = cart => ({
+  type: SET_GUEST_CART,
+  cart
+})
+
+//thunk
+
+export const setGuestCart = () => {
+  return dispatch => {
+    const data = JSON.parse(localStorage.guestCart)
+    dispatch(initialCart(data.results))
+  }
+}
+
+//reducer
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case SET_GUEST_CART:
+      return action.cart
+    default:
+      return state
+  }
+}
